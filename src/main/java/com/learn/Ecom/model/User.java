417a0dae93ef172175490a6dcea3eede91a44c0a
sritharun00i <vuppusritharun@gmail.com>
2026-01-03 21:56:20 +0530

    Fix User model before rebase

--- src/main/java/com/learn/Ecom/model/User.java
+++ src/main/java/com/learn/Ecom/model/User.java
@@ -4,6 +4,10 @@ import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
+
+import java.time.LocalDateTime;
 
 @Entity
 @Table(name = "users")
@@ -25,6 +29,14 @@ public class User {
     private String name;
 
     private String role;
+
+    @CreationTimestamp
+    @Column(name = "created_at", updatable = false)
+    private LocalDateTime createdAt;
+
+    @UpdateTimestamp
+    @Column(name = "updated_at")
+    private LocalDateTime updatedAt;
 
     public User safeCopy() {
         User user = new User();
